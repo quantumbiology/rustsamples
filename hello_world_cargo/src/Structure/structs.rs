00d481864b2da858507be62d66a0dbc66a8ed2ed
Piyush Soni <82703231+reactivequantumblockchain@users.noreply.github.com>
2021-04-21 23:41:10 +0530

    Update Struct Comments
    Update Struct Comments

--- hello_world_cargo/src/Structure/structs.rs
+++ hello_world_cargo/src/Structure/structs.rs
@@ -5,11 +5,13 @@ struct Color {
   blue: u8
 }
 fn main() {
-
+      
+    // Getting value from the Struct
     let mut bg = Color {red:255 , green : 155, blue : 65};
     
     println!("Original values are {}, {}, {}" , bg.red, bg.green, bg.blue);
-
+    
+    // Changing the value of struct
     bg.blue = 45;
     println!("Changed values are {}, {}, {}" , bg.red, bg.green, bg.blue);
 
